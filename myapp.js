96f17dd282fc69467bfa769610ada118eeab909c
Scott Robinson <scott.home@live.com>
2018-07-24 08:20:48 -0700

    Fuctions Lesson

--- myapp.js
+++ myapp.js
@@ -14,3 +14,23 @@
 
 // console.log ("string" , 10.11, true, false, null, num);
 
+//function exercise
+
+function addTwoNumbers (num1, num2){
+    console.log (num1 + num2);
+    return (num1 + num2);
+}
+
+function makeCoffee (sugar, milk){
+    var instructions = "Boil Water,";
+    instructions += " Pour into Cup,";
+    instructions += " add coffee granules,";
+    instructions += " add " + sugar + " spoons of sugar,";
+    instructions += " add " + milk + "%milk.";
+    return instructions
+}
+
+
+
+console.log(addTwoNumbers (2,4));
+console.log(makeCoffee(2,2));
