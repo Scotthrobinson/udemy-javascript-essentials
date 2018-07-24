a9d9fa6b6e17f134697412751330ce1a8a3704a9
Scott Robinson <scott.home@live.com>
2018-07-24 08:15:21 -0700

    Logging Exercise added

---
+++ myapp.js
@@ -0,0 +1,16 @@
+//logging exercise
+
+// console.log( "Hello world 1" );
+
+// console.log(200);
+
+// console.log( true);
+
+// console.log (null);
+
+// var num = 10;
+
+// console.log (num);
+
+// console.log ("string" , 10.11, true, false, null, num);
+
