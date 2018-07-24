f60e02ae0a466875d7d031594328b7539cf39b7d
Scott Robinson <scott.home@live.com>
2018-07-24 11:16:34 -0700

    Embedded Objects and Arrays

---
+++ myapp-embedded.js
@@ -0,0 +1,17 @@
+var car = {
+    make : "Volvo",
+    speed : 160,
+    engine : {
+        size: 2.0,
+        make: "BMW",
+        fuel: "Petrol",
+        pistons: [ {Maker : "BMW"},{Maker : "BMW"}]
+    },
+    drive : function(){return "Drive";}
+};
+
+var array = [ "String",
+                100, 
+                [ "Embed", 200],
+                { car : "Ford"}
+            ];
