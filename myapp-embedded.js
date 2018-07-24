c7ad738d6ce5edbcd0b1c673a5d9a9379a0817ed
Scott Robinson <scott.home@live.com>
2018-07-24 13:59:07 -0700

    Member and Array Access

--- myapp-embedded.js
+++ myapp-embedded.js
@@ -13,5 +13,25 @@ var car = {
 var array = [ "String",
                 100, 
                 [ "Embed", 200],
-                { car : "Ford"}
+                { car : "Ford"},
+                function() {return "drive";}
             ];
+
+
+//Member Access
+console.log(car.make);
+console.log(car.engine.size);
+console.log(car.drive());
+
+//Array Access
+console.log(car.engine.pistons[0]);
+//Member inside a array
+console.log(car.engine.pistons[0].Maker);
+//Access Array
+console.log(array[0]);
+//Access function inside an array
+console.log(array[4]());
+
+
+
+
